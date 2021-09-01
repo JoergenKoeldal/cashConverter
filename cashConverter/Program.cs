3d9ad3bf032c08dac8883485ae27efe34f3dac41
JooergenKoeldal <joer1894@stud.kea.dk>
2021-09-01 16:04:16 +0200

    Add project files.

---
+++ cashConverter/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace cashConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter amount of money you would like to deposit in DKK");
+            string amountToExhange = Console.ReadLine();
+            Console.WriteLine("Enter what type of currency code, you would like to exhange to");
+            string codeChosen = Console.ReadLine();
+
+
+
+
+            XDocument doc = XDocument.Load("https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=da");
+
+            var currencies = doc.XPathSelectElements("//currency");
+
+            List<Currency> listOfCurrencies = new List<Currency>();
+
+            foreach (XElement element in currencies)
+            {
+
+                string code = element.Attribute("code").Value;
+
+                string desc = element.Attribute("desc").Value;
+
+                decimal rate = Decimal.Parse(element.Attribute("rate").Value);
+
+
+                //Lavet et object af currency
+                Currency currency = new Currency();
+
+                //Bruger setter metoderne fra klassen til at give en value
+                currency.Code = code;
+                currency.Desc = desc;
+                currency.Rate = rate;
+
+                //Tilføjer alle vores currency fra XML filen til en liste som objekter
+                listOfCurrencies.Add(currency);
+
+            }
+
+            foreach (Currency currency in listOfCurrencies)
+            {
+                decimal exhangedAmount = 0;
+
+                if (codeChosen == currency.Code)
+                {
+                    exhangedAmount = Decimal.Parse(amountToExhange) / (currency.Rate / 100);
+                    Console.WriteLine(exhangedAmount);
+                    return;
+                }
+
+
+
+            }
+
+        }
+    }
+
+    class Currency
+    {
+        private string code;
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        private string desc;
+        public string Desc
+        {
+            get { return desc; }
+            set { desc = value; }
+        }
+
+        private decimal rate;
+        public decimal Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
